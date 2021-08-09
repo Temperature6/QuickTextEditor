dddea1e9eb49925afcff5a77d8c548cc7ddf5344
Temperature6 <61113523+Temperature6@users.noreply.github.com>
2021-08-09 13:19:33 +0800

    Add files via upload

---
+++ src/mainwindow.cpp
@@ -0,0 +1,106 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QTextEdit>
+#include <QDebug>
+#include <QClipboard>
+#include <QFileDialog>
+#include <QMessageBox>
+#include <QDesktopServices>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    //图标
+    setWindowIcon(QIcon(":appicon.ico"));
+    //ui->copy_btn->setIcon(QIcon(":copy_text.ico"));
+    ui->clear_button->setIcon(QIcon(":delete_text.ico"));
+    //菜单栏样式表
+    ui->menuBar->setStyleSheet("QMenuBar{background-color:#e8e8e8;}");
+    //槽和函数链接
+    void(QSpinBox::*changed)(int) = &QSpinBox::valueChanged;
+    connect(ui->copy_btn, &QPushButton::clicked, this, &MainWindow::CopyText);
+    connect(ui->textsize, changed, this, &MainWindow::ChangeTextSize);
+    connect(ui->_save, &QAction::triggered, this, &MainWindow::SaveText);
+    connect(ui->copy_all, &QAction::triggered, this, &MainWindow::ChangeWayCopy);
+    connect(ui->cut_all, &QAction::triggered, this, &MainWindow::ChangeWayCut);
+    connect(ui->open_github, &QAction::triggered, this, &MainWindow::OpenGitHub);
+
+    if (copy)
+    {
+        ChangeWayCopy();
+    }
+    else
+    {
+        ChangeWayCut();
+    }
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::CopyText()
+{
+    QClipboard *clipboard = QApplication::clipboard(); //系统剪贴板指针
+    QString str = ui->textEdit->toPlainText();
+    clipboard->setText(str);
+    if (!copy) //复制
+    {
+        ui->textEdit->setText("");
+    }
+    return;
+}
+
+void MainWindow::ChangeTextSize()
+{
+    QFont font = ui->textEdit->font();
+    font.setPointSize(ui->textsize->value());
+    ui->textEdit->setFont(font);
+    return;
+}
+
+void MainWindow::SaveText()
+{
+    QString fileName = QFileDialog::getSaveFileName(this,tr("保存文本"),"",tr("文本文档(*.txt)"));
+    QFile file(fileName);
+
+    if (!file.open(QIODevice::WriteOnly|QIODevice::Text))
+    {
+        return;
+    }
+    else
+    {
+        QTextStream stream(&file);
+        stream << ui->textEdit->toPlainText();
+        stream.flush();
+        file.close();
+    }
+    return;
+}
+
+void MainWindow::ChangeWayCopy()
+{
+    //由剪切到复制
+    ui->copy_btn->setText("复制全部");
+    copy = true;
+    ui->copy_btn->setIcon(QIcon(":copy_text.ico"));
+    return;
+}
+
+void MainWindow::ChangeWayCut()
+{
+    //由复制到剪切
+    ui->copy_btn->setText("剪切全部");
+    copy = false;
+    ui->copy_btn->setIcon(QIcon(":cut.ico"));
+    return;
+}
+
+void MainWindow::OpenGitHub()
+{
+    QDesktopServices::openUrl(QUrl(QString("https://github.com/Temperature6/QuickTextEditor")));;
+    return;
+}
